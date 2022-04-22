48d02cf305f80d7cafde4c7dec85dbffc3e17122
kamilla-mylena <kamilla.antunes@ciandt.com>
2022-04-22 00:01:51 -0300

    [feature] Arquivos de fontes e de cores

---
+++ pokemon-mentoria-app/Design System/Settings/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  pokemon-mentoria-app
+//
+//  Created by kamilla.antunes on 21/04/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundTextFieldColor = UIColor(red: 0.77, green: 0.77, blue: 0.77, alpha: 0.90)
+    static let textFieldColor = UIColor(red: 0.46, green: 0.46, blue: 0.46, alpha: 1.00)
+}
---
+++ pokemon-mentoria-app/Design System/Settings/Fonts.swift
@@ -0,0 +1,19 @@
+//
+//  Fonts.swift
+//  pokemon-mentoria-app
+//
+//  Created by kamilla.antunes on 21/04/22.
+//
+
+import UIKit
+
+extension UIFont {
+    static let oswaldFontH1 = UIFont(name: "Oswald", size: 68)
+    static let oswaldFontH2 = UIFont(name: "Oswald", size: 42)
+    static let oswaldFontH3 = UIFont(name: "Oswald", size: 26)
+    static let oswaldFontH4 = UIFont(name: "Oswald", size: 16)
+    static let josefinSansFontParagraph = UIFont(name: "Oswald", size: 16)
+    static let josefinSansFontSmall = UIFont(name: "Oswald", size: 14)
+    static let josefinSansFontCaption = UIFont(name: "Oswald", size: 12)
+    
+}
